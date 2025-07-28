51c184fc7a3507b8320a432f6c455fee2ca34260
piryth <arnaud.endignous@etu.univ-grenoble-alpes.fr>
2025-07-28 08:34:44 +0200

    fix(): sitemap error handling

--- front-blog/app/sitemap.ts
+++ front-blog/app/sitemap.ts
@@ -9,42 +9,59 @@ type Post = {
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
-  const res = await fetch(`${config.apiUri}/api/v1/posts`, {
-    headers: {'x-api-key': config.apiKey},
-  });
+  try {
 
-  logger.info("Fetching blog posts for ssg")
+    const res = await fetch(`${config.apiUri}/api/v1/posts`, {
+      headers: {'x-api-key': config.apiKey},
+    });
 
-  const posts = await res.json() as Post[];
+    if(!res.ok) {
+      throw new Error(`Failed to fetch posts: ${res.status} ${res.statusText}`);
+    }
 
-  logger.info(posts.length + " posts found")
+    logger.info("Fetching blog posts for ssg")
 
-  const postsUrls = posts.map(post => ({
-    url: `${config.frontUri}/blog/${post.slug}`,
-    lastModified: new Date(post.created_at),
-    changeFrequency: 'daily' as 'daily',
-    priority: 0.7,
-  }));
+    const posts = await res.json() as Post[];
 
-  return [
-    {
-      url: `${config.frontUri}`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: `${config.frontUri}/about`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${config.frontUri}/blog`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
-    ...postsUrls
-  ]
+    logger.info(posts.length + " posts found")
+
+    const postsUrls = posts.map(post => ({
+      url: `${config.frontUri}/blog/${post.slug}`,
+      lastModified: new Date(post.created_at),
+      changeFrequency: 'daily' as 'daily',
+      priority: 0.7,
+    }));
+
+    return [
+      {
+        url: `${config.frontUri}`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: `${config.frontUri}/about`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: `${config.frontUri}/blog`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      ...postsUrls
+    ]
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(`Error generating sitemap: ${error.message}`);
+    } else {
+      logger.error(`Error generating sitemap: ${String(error)}`);
+    }
+    // Stop the build process
+    process.exitCode = 1;
+    throw error;
+  }
+
 }
